fbb5a066fbf2138736fad4fdc6244b056b51769a
leoOH5 <bbin0409@naver.com>
2025-08-24 19:23:07 +0900

    chore: initial commit

---
+++ src/main/java/com/example/SechongMaru/SechongMaruApplication.java
@@ -0,0 +1,13 @@
+package com.example.SechongMaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SechongMaruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SechongMaruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SechongMaru/SechongMaruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SechongMaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SechongMaruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
